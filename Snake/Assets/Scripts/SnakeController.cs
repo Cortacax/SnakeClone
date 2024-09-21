5631050d5b8e410b7bc6b7f2ff37f8369864ee33
Cortacax <emiroz18@hotmail.com>
2024-09-21 19:04:51 +0300

    Project Update

--- Snake/Assets/Scripts/SnakeController.cs
+++ Snake/Assets/Scripts/SnakeController.cs
@@ -1,23 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEditorInternal;
 using UnityEngine;
 
 public class SnakeController : MonoBehaviour
 {
-    private Vector2 _areaLimit = new Vector2(13, 2);
+    private Vector2 _areaLimit = new Vector2(13, 24);
 
 
     [SerializeField] private GameObject food;
     [SerializeField] private GameObject tailPrefab;
     [SerializeField] private float speed = 1;
+    [SerializeField] private TextMeshPro textScore;
+    [SerializeField] private TextMeshPro textGameOver;
     private Vector2 _direction = Vector2.down;
+    private List<Transform> _snake = new List<Transform> ();
+    private int Score
+    {
+        get => _score;
+        set 
+        {
+            _score=value;
+            textScore.text = _score.ToString();
+        }
+    }
+    private int _score;
+    private bool _grow;
 
     private void Start()
     {
+        textGameOver.enabled = false;
+        Score = 0;
         ChangePositionFood();
         StartCoroutine(Move());
+
+        _snake.Add(transform);
     }
 
     private void Update()
@@ -45,6 +64,12 @@ public class SnakeController : MonoBehaviour
     {
         while (true)
         {
+
+            for (int i = _snake.Count-1; i > 0; i--)
+            {
+                _snake[i].position = _snake[i-1].position;
+            }
+
 
             var position = transform.position;
             position += (Vector3)_direction;
@@ -75,22 +100,46 @@ public class SnakeController : MonoBehaviour
 
         var tail = Instantiate(tailPrefab);
         ChangePositionFood();
-
+        _snake.Add(tail.transform);
+        _snake[_snake.Count - 1].position = _snake[_snake.Count - 2].position;
+        
 
     }
 
     private void ChangePositionFood()
     {
-        var x = (int)Random.Range(1, _areaLimit.x);
-        var y = (int)Random.Range(1, _areaLimit.y);
-        food.transform.position = new Vector3(x, y, 0);
+        Vector2 newFoodPosition;
+        
+        do
+        {
+            var x = (int)Random.Range(1, _areaLimit.x);
+            var y = (int)Random.Range(1, _areaLimit.y);
+            newFoodPosition = new Vector2(x, y);
+        } while (!CanFoodSpawn(newFoodPosition));
+
+
+
+        food.transform.position = newFoodPosition;
 
     }
-
+       private bool CanFoodSpawn(Vector2 newPosition)
+    {
+        foreach (var item in _snake)
+        {
+            var x = Mathf.RoundToInt(item.position.x);
+            var y = Mathf.RoundToInt(item.position.y);
+            if (new Vector2(x,y) == newPosition)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 
     private void Death()
     {
-
+        textGameOver.enabled = true;
+        StopAllCoroutines();
     }
 
 }
